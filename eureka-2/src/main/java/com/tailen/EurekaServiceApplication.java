6caadf29a16ffd71169d06c5acc84e9df8c5d588
tailen <zhaotailin@chaoxing.com>
2021-07-07 09:54:25 +0800

    eureka

--- eureka-2/src/main/java/com/tailen/EurekaServiceApplication.java
+++ eureka-2/src/main/java/com/tailen/EurekaServiceApplication.java
@@ -3,6 +3,7 @@ package com.tailen;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
