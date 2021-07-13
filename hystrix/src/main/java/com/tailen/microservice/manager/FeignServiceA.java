8132c87a24c27d5abeea7e008bc356a25ba0b44d
tailen <zhaotailin@chaoxing.com>
2021-07-13 10:03:33 +0800

    1

--- hystrix/src/main/java/com/tailen/microservice/manager/FeignServiceA.java
+++ hystrix/src/main/java/com/tailen/microservice/manager/FeignServiceA.java
@@ -1,7 +1,7 @@
 package com.tailen.microservice.manager;
 
 import com.tailen.microservice.manager.errorBack.FeignServiceAFallback;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
